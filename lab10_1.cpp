33676da6692863ec5b71fa9f13d8470991a6c992
Pitipat <pitipat_fu@cmu.ac.th>
2021-12-14 16:17:30 +0700

    commit lap10_1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main(){	
+
+	double l , r , y ;
+
 	cout << "Enter initial loan: ";
+	cin >> l ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> r ;
 	cout << "Enter amount you can pay per year: ";
+	cin >> y ;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +26,45 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+
+	int i = 0 ;
+	double I , T , N ;
+
+	while ( l > 0.00 )
+	{
+		cout << fixed << setprecision(2); 
+		//EndOfYear#
+		cout << setw(13) << left << 1+i; 
+		//PrevBalance
+		cout << setw(13) << left << l ;
+		//Interest
+		I = l*(r/100) ;
+		cout << setw(13) << left << I ;
+		//Total  
+		T = l+I ;   
+		cout << setw(13) << left << T ;
+
+		//Payment   
+		if( l > y )  {
+			cout << setw(13) << left << y ;
+		}else {
+			if ( T > y ) {
+				cout << setw(13) << left << y ;
+			}else if( l < y ){
+				y = T ;
+				cout << setw(13) << left << y ;
+			}
+			
+		}
+
+		//NewBalance
+		N = T-y ;
+		cout << setw(13) << left << N  ;
+		cout << "\n";	 
+		l = N ;
+
+		i++ ;
+	}
 	
 	return 0;
 }
